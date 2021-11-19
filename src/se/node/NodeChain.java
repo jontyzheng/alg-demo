9d35707f64ea6dc8c0056f8a3335025501e9b0a8
jonty <jontyzhen@gmail.com>
2021-11-20 00:02:50 +0800

    链--给链换上一个新头

--- src/se/node/NodeChain.java
+++ src/se/node/NodeChain.java
@@ -2,6 +2,7 @@ package se.node;
 
 /**
  * 链表就像一个个引用
+ * reference: alg4 1.3.3.2 构造链表
  * */
 public class NodeChain {
     public static void main(String[] args) {
@@ -12,11 +13,28 @@ public class NodeChain {
         second.next = third;    // 只用 second 拼接下一个域
 
         // 结果：head 输出了整条链
+        Node tmpHead = head;
+        while (tmpHead != null) {
+            System.out.println(tmpHead.item);
+            tmpHead = tmpHead.next;
+        }
+
+        System.out.println("----------");
+
+        /**
+         * 在头部加上一个结点
+         * */
+        Node oldHead = head;
+        // (先将 head 保存在 oldHead 里面)
+        Node first = new Node("not");
+        // (构造一个新的结点 值为 not)
+        first.next = oldHead;
+        head = first;
+        // 此时，新的头接上旧的身体，然后再提着新的头走
+        System.out.println("看看链条有没有更新：");
         while (head != null) {
             System.out.println(head.item);
             head = head.next;
         }
-
-
     }
 }
