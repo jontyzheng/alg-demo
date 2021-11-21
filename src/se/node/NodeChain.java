9f1070b89f171cb5e19a33eac093fd7895781ff0
jonty <jontyzhen@gmail.com>
2021-11-21 21:21:53 +0800

    链--通过修改链开始结点的引用，删除原来的开始结点

--- src/se/node/NodeChain.java
+++ src/se/node/NodeChain.java
@@ -1,7 +1,7 @@
 package se.node;
 
 /**
- * 链表就像一个个引用
+ * 链表上的结点就像一个个引用
  * reference: alg4 1.3.3.2 构造链表
  * */
 public class NodeChain {
@@ -12,6 +12,8 @@ public class NodeChain {
         Node third = new Node("or");
         second.next = third;    // 只用 second 拼接下一个域
 
+
+        System.out.println("原链条：");
         // 结果：head 输出了整条链
         Node tmpHead = head;
         while (tmpHead != null) {
@@ -29,12 +31,26 @@ public class NodeChain {
         Node first = new Node("not");
         // (构造一个新的结点 值为 not)
         first.next = oldHead;
+        // 将原来的链接上新结点后面
         head = first;
-        // 此时，新的头接上旧的身体，然后再提着新的头走
-        System.out.println("看看链条有没有更新：");
-        while (head != null) {
-            System.out.println(head.item);
-            head = head.next;
+        // 此时，新节点为首的链恰好是满足的链
+        System.out.println("看看链条有没有获得新的头节点：");
+        Node tmpHead2 = head;
+        while (tmpHead2 != null) {
+            System.out.println(tmpHead2.item);
+            tmpHead2 = tmpHead2.next;
+        }
+
+        /**
+         * 删除链的开始节点(修改链的头节点)
+         * */
+        System.out.println("----------");
+        System.out.println("看看链条去除头节点之后：");
+        head = head.next;
+        Node tmpHead3 = head;
+        while (tmpHead3 != null) {
+            System.out.println(tmpHead3.item);
+            tmpHead3 = tmpHead3.next;
         }
     }
 }
